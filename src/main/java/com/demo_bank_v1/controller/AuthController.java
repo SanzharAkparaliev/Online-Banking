ac527bbacab0fcc109a9d4c6520d2008c036e1f3
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-10-27 22:32:21 +0600

    redirecting

--- src/main/java/com/demo_bank_v1/controller/AuthController.java
+++ src/main/java/com/demo_bank_v1/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 
@@ -65,5 +66,12 @@ public class AuthController {
         httpSession.setAttribute("authenticated",true);
 
         return "redirect:/app/dashboard";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session, RedirectAttributes redirectAttributes){
+        session.invalidate();
+        redirectAttributes.addFlashAttribute("logged_out","Logged out successfully");
+        return "redirect:/login";
     }
 }
--- src/main/webapp/WEB-INF/jsp/components/incl/header.jsp
+++ src/main/webapp/WEB-INF/jsp/components/incl/header.jsp
@@ -14,10 +14,10 @@
             <li><a href="">Transaction History</a></li>
         </nav>
         <div class="text-white ml-auto display-name ">
-            <i class="fa fa-circle text-success mr-2"></i>Welcome: <span style="font-weight: 200; margin-right: 30px;">Sanzhar Taalaibek uulu</span>
+            <i class="fa fa-circle text-success mr-2"></i>Welcome: <span style="font-weight: 200; margin-right: 30px;">${user.first_name} ${user.last_name}</span>
         </div>
 
-        <a href="" class="btn btn-sm text-white">
+        <a href="/logout" class="btn btn-sm text-white">
             <i class="fa fa-sign-out mr-2" aria-hidden="true"></i>Sign out
         </a>
     </div>
