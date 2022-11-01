6be9a155d953a0a6f5273709134c86276dbd0522
Sanzhar <81224476+SanzharAkparaliev@users.noreply.github.com>
2022-11-01 11:17:38 +0600

    bug fixes

--- src/main/java/com/demo_bank_v1/repository/PaymentHistoryRepository.java
+++ src/main/java/com/demo_bank_v1/repository/PaymentHistoryRepository.java
@@ -13,6 +13,6 @@ import java.util.List;
 public interface PaymentHistoryRepository extends JpaRepository<PaymentHistory,Integer> {
 
     @Query(value = "SELECT p.payment_id,a.account_id,u.user_id, p.beneficiary, p.beneficiary_acc_no, p.amount,p.status,p.reference_no,p.reason_code,p.created_at from " +
-            "payment p inner join account a on p.account_id = a.account_id INNER JOIN `user` u on a.account_id = u.user_id = :user_id", nativeQuery = true)
+            "payment p inner join account a on p.account_id = a.account_id INNER JOIN `user` u on a.user_id = u.user_id = :user_id", nativeQuery = true)
     List<PaymentHistory> getPaymentRecordsById(@Param("user_id")int user_id);
 }
